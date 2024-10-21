cea69db32f714da9a80e3f121039f48b0a271974
giovannisouzal <giovannisouzal2005@gmail.com>
2024-10-21 18:49:08 -0300

    paginas criadas

---
+++ cp6front/src/app/portfolio/pedro/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { Tipos } from "@/type";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function pedro() {
+
+    
+
+
+    return (
+
+    )
+}
