452e87ab4419451294773b7897604948952fcdf0
GiovannisouzaL <giovannisouzal2005@gmail.com>
2024-10-22 08:29:52 -0300

    Alterações no css e as páginas

--- cp6front/src/components/Menu/Menu.tsx
+++ cp6front/src/components/Menu/Menu.tsx
@@ -2,10 +2,17 @@ import Link from "next/link";
 
 export default function Menu() {
   return (
-    <nav>
-        <ul>
-            <li> <Link href="/">Home</Link> </li>
+    <main className="menu">
+      <nav>
+        <ul className="flex gap-5 justify-center">
+          <li><Link href="/">Home |</Link></li>
+          <li><Link href="/giovanni">Giovanni |</Link></li>
+          <li><Link href="/vitor">Vitor |</Link></li>
+          <li><Link href="/pedro">Pedro |</Link></li>
+          <li><Link href="/victor">Victor |</Link></li>
+          <li><Link href="/mariana">Mariana |</Link></li>
         </ul>
-    </nav>
-  )
+      </nav>
+    </main>
+  );
 }
--- cp6front/src/components/Rodape/Rodape.tsx
+++ cp6front/src/components/Rodape/Rodape.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 export default function Rodape() {
-    return (
-      <footer>
-          <h2>Rodapé</h2>
-          <p>Desenvolvido por <span>pf0670</span></p>
-      </footer>
-    )
-  }
-  
+  return (
+    <footer className="rodape">
+      <h2>Integrantes:</h2>
+      <li> <Link href="https://github.com/GiovanniSouzaL">Giovanni de Souza</Link> </li>
+      <li> <Link href="https://github.com/ovitortadeu">Vitor Tadeu</Link> </li>
+      <li> <Link href="https://github.com/PBPedro">Pedro Paulo</Link> </li>
+      <li> <Link href="https://github.com/victoragrela">Victor Agrela</Link> </li>
+      <li> <Link href="https://github.com/PBPedro">Mariana Roberti</Link> </li>
+    </footer>
+  )
+}
