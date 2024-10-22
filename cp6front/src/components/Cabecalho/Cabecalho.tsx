80db10c0599648cff2b5c2da4feff97ee8526cbf
GiovannisouzaL <giovannisouzal2005@gmail.com>
2024-10-22 08:52:10 -0300

    alterações no global.css

--- cp6front/src/components/Cabecalho/Cabecalho.tsx
+++ cp6front/src/components/Cabecalho/Cabecalho.tsx
@@ -3,8 +3,8 @@ import Menu from "../Menu/Menu";
 export default function Cabecalho() {
   return (
     <header className="cabecalho">
-        <h1>Cabecalho</h1>
-        <Menu/>
+      <h1>Páginas</h1>
+      <Menu />
     </header>
   )
 }
