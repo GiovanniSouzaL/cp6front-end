194b85c0ca7016c2fdabf9750881cac1fb7674d9
giovannisouzal <giovannisouzal2005@gmail.com>
2024-10-27 15:19:42 -0300

    adição legal

--- cp6front/src/app/page.tsx
+++ cp6front/src/app/page.tsx
@@ -1,14 +1,7 @@
 export default function Home() {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold">Visão geral das avaliações em front-end</h1>
-      <h2 className="text-2xl font-bold">1º semestre:</h2>
-      <p className="font-bold">CPS</p>
-      <p>cp1: 7,5/10</p>
-      <p>cp2: não realizado</p>
-      <p>cp3: não realizado</p>
-      <p className="font-bold">Challenger-sprints</p>
-      <p>ch1: 8/10</p>
+      <h1 className="text-2xl font-bold text-center">Sobre mim</h1>
     </div>
   );
 }
--- cp6front/src/components/rodape.tsx
+++ cp6front/src/components/rodape.tsx
@@ -1,4 +1,4 @@
 export default function Rodape() {
-    return <footer className="p-4 bg-gray-800 text-white">© 2024 Portifólio de Giovanni</footer>;
+    return <footer className="p-10 bg-gray-800 text-white text-2xl ">© 2024 Portifólio de Giovanni</footer>;
   }
   
