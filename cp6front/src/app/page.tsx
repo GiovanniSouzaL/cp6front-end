ecbc3fe54b83d741dff8adb2993f5f27d46f9a52
giovannisouzal <giovannisouzal2005@gmail.com>
2024-10-27 15:24:55 -0300

    giovanni deletado (morto)

--- cp6front/src/app/page.tsx
+++ cp6front/src/app/page.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold text-center">Sobre mim</h1>
+      <Image
+                src="../public/giovanni.jpg"
+                alt="Imagem de Giovanni"
+                width={1920}
+                height={1080}
+            />
+      
     </div>
   );
 }
