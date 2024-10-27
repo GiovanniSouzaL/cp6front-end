a9a84665f1e2453b78d6c8898ab399868409ae6a
victoragrela <victoragrela2@gmail.com>
2024-10-27 17:58:30 -0300

    página cps pronta

--- cp6front/src/app/page.tsx
+++ cp6front/src/app/page.tsx
@@ -4,11 +4,10 @@ export default function Home() {
       <h1 className="text-4xl font-bold mb-4">Sobre nós</h1>
       <p className="text-2xl font-bold mb-4">Giovanni</p>
       <p className="text-lg max-w-lg text-justify">
-        Olá! Sou Giovanni, estudante de Análise e Desenvolvimento de Sistemas na FIAP (1TDSPH). Estou em uma jornada para me tornar desenvolvedor Full Stack, com foco em tecnologias como Java, Python, Next.js e bancos de dados. Busco aprimorar minhas habilidades tanto no back-end quanto no front-end para construir aplicações completas e funcionais. Sigo aprendendo e aplicando as melhores práticas de desenvolvimento, sempre focado em entregar soluções robustas e eficientes.<br/><br/>
-      </p>
+        Olá! Sou Giovanni, estudante de Análise e Desenvolvimento de Sistemas na FIAP (1TDSPH). Estou em uma jornada para me tornar desenvolvedor Full Stack, com foco em tecnologias como Java, Python, Next.js e bancos de dados. Busco aprimorar minhas habilidades tanto no back-end quanto no front-end para construir aplicações completas e funcionais. Sigo aprendendo e aplicando as melhores práticas de desenvolvimento, sempre focado em entregar soluções robustas e eficientes.</p><br/>
       <h2 className="text-2xl font-bold mb-4">Victor</h2>
       <p className="text-lg max-w-lg text-justify">
-        Olá! Sou Victor, estudante de Análise e Desenvolvimento de Sistemas na FIAP, e estou trilhando meu caminho para me tornar um desenvolvedor Full Stack. Meu foco é dominar tecnologias como Java, Python, Next.js e bancos de dados, com o objetivo de aprimorar minhas habilidades tanto no back-end quanto no front-end. Busco sempre aplicar as melhores práticas de desenvolvimento para criar aplicações robustas, completas e funcionais, e estou comprometido em evoluir constantemente para entregar soluções eficientes e de alta qualidade.<br/><br/></p>
+        Olá! Sou Victor, estudante de Análise e Desenvolvimento de Sistemas na FIAP, e estou trilhando meu caminho para me tornar um desenvolvedor Full Stack. Meu foco é dominar tecnologias como Java, Python, Next.js e bancos de dados, com o objetivo de aprimorar minhas habilidades tanto no back-end quanto no front-end. Busco sempre aplicar as melhores práticas de desenvolvimento para criar aplicações robustas, completas e funcionais, e estou comprometido em evoluir constantemente para entregar soluções eficientes e de alta qualidade.</p><br/>
     </div>
   );
 }
