ed2ed9c9062438bcc852338b82bdcf09a6158a92
giovannisouzal <giovannisouzal2005@gmail.com>
2024-10-27 16:04:37 -0300

    a

--- cp6front/src/app/checkpoints/page.tsx
+++ cp6front/src/app/checkpoints/page.tsx
@@ -4,11 +4,11 @@ export default function CheckPoints() {
         <h2 className="text-2xl font-bold">CheckPoints</h2>
         <p>Listagem de todas as avaliações de CheckPoints realizadas no primeiro semestre.</p>
         <p className="text-xl font-bold mb-4">Giovanni</p>
-        <p>cp1: 7.5/10</p>
-        <p>Nesse cp foi pedido</p>
-                <a
-          href="https://github.com/GiovanniSouzaL/CP1" target="_blank" rel="noopener noreferrer" className="inline-block mt-4 text-blue-500 underline hover:text-blue-700 transition duration-200">Ver repositório no GitHub
+        <a
+        href="https://github.com/GiovanniSouzaL/CP1" target="_blank" rel="noopener noreferrer" className="inline-block mt-4 text-blue-500 underline hover:text-blue-700 transition duration-200">CP1: 7.5/10
         </a>
+        <p>Nesse cp foi pedido</p>
+
       </div>
     );
   }
