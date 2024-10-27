b8d2b2141e543dbf2e2847f97f6e0f4a88eb45c8
Vitor Tadeu <souavitor6@gmail.com>
2024-10-27 18:50:37 -0300

    Co-authored-by: Giovanni de Souza <GiovanniSouzaL@users.noreply.github.com>

--- cp6front/src/app/checkpoints/page.tsx
+++ cp6front/src/app/checkpoints/page.tsx
@@ -1,3 +1,4 @@
+
 export default function CheckPoints() {
     return (
       <div className="p-4">
@@ -7,26 +8,31 @@ export default function CheckPoints() {
         <p>Nesse CP foi pedido a criação de um site com todos os cursos da fiap + suas paginas favoritas</p>
         <a href="https://github.com/GiovanniSouzaL/CP1" target="_blank" rel="noopener noreferrer" className="inline-block mt-4 text-blue-500 no-underline hover:text-blue-700 transition duration-200">Giovanni CP1: 7.5/10</a><br/>
         <a href="https://github.com/victoragrela/front_cp1" target="_blank" rel="noopener noreferrer" className="inline-block mt-4 text-blue-500 no-underline hover:text-blue-700 transition duration-200">Victor CP1: 8.5/10</a><br/>
+        <a href="https://github.com/ovitortadeu/cp1" target="_blank" rel="noopener noreferrer" className="inline-block mt-4 text-blue-500 no-underline hover:text-blue-700 transition duration-200">Vitor CP1: 7.5/10</a>
         
         <p className="text-xl font-bold mb-4">Cp2</p>
         <p>Nesse CP pediu um protótipo no figma e cada grupo da sala ficou com uma parte para realizar e no final cada representante do grupo uniu as partes </p>
         <p>Giovanni CP2: não realizado</p>
         <p>Victor CP2: 10/10</p>
+        <p>Vitor CP2: 10/10</p>
 
         <p className="text-xl font-bold mb-4">Cp3</p>
         <p>Nesse CP foi pedido para fazer um sistema de login e logout em um site</p>
         <p>Giovanni CP3: não realizado</p>
         <p>Victor CP3: não realizado</p><br/>
+        <p>Vitor CP3: não realizado</p>
 
         <p className="text-xl font-bold mb-4">Cp4</p>
         <p>Nesse CP foi pedido fazer um site com react para fazer uma livraria e SPA</p>
         <p>Giovanni CP4: não realizado</p>
         <p>Victor CP4: não realizado</p><br/>
+        <p>Vitor CP4: não realizado<p/>
 
         <p className="text-xl font-bold mb-4">Cp5</p>
         <p>Nesse CP foi pedido um site com paginas sobre o livro de Immanuel Velikovsky  Worlds in collision</p>
         <a href="https://github.com/GiovanniSouzaL/cp5_front" target="_blank" rel="noopener noreferrer" className="inline-block mt-4 text-blue-500 no-underline hover:text-blue-700 transition duration-200">Giovanni CP5: 4.5/10</a><br/>
         <a href="https://github.com/GiovanniSouzaL/cp5_front" target="_blank" rel="noopener noreferrer" className="inline-block mt-4 text-blue-500 no-underline hover:text-blue-700 transition duration-200">Victor CP5: 5/10</a><br/>
+        <a href="https://github.com/felipesora/cp5" target="_blank" rel="noopener noreferrer" className="inline-block mt-4 text-blue-500 no-underline hover:text-blue-700 transition duration-200">Vitor e Pedro: 6.5/10</a>
       </div>
     );
   }
